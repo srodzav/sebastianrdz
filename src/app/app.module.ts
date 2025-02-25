0849bd576d5f831b137048e3cf942204bd65df93
srodzav <mrjchzz@gmail.com>
2025-02-24 18:05:22 -0600

    style: change current job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { AboutMeComponent } from './pages/about-me/about-me.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,10 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  isDarkMode: boolean = false;
+
+  ngOnInit() {
+    this.isDarkMode = localStorage.getItem('darkMode') === 'true';
+    this.updateBodyClass();
+  }
 
   resume() : void {
     window.open('/assets/files/resume.pdf', '_blank');
+  }
+
+  toggleTheme() : void {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+    this.updateBodyClass();
+  }
+
+  updateBodyClass(): void {
+    if (this.isDarkMode) {
+      document.body.classList.add('bg-dark', 'text-light');
+      document.body.classList.remove('bg-light', 'text-dark');
+    } else {
+      document.body.classList.add('bg-light', 'text-dark');
+      document.body.classList.remove('bg-dark', 'text-light');
+    }
   }
 
 }
